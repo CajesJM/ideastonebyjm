44d175088c7fb627c8bdc7daacc23bbf5806d929
CajesJM <markcajes24@gmail.com>
2025-08-21 16:19:20 +0800

    Update routing and Vite config for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import IdeaGenerator from './Components/IdeaGenerator.jsx';
 import HomePage from './HomePage.jsx';
 function App() {
   return (
     <>
-  <BrowserRouter>
+  <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/Generator" element={<IdeaGenerator />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
 
       
     </>

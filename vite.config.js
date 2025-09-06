deced4f2fd437e92fb95b1c8ed154ea2eb1a5566
CajesJM <markcajes24@gmail.com>
2025-09-06 19:07:31 +0800

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/ideastonebyjm/',
   plugins: [react()],
- base: '/IdeaStone/',
-});
+  build: {
+    outDir: 'docs'
+  }
+})
